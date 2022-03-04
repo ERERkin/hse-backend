1e874839487db68a54481c39109851ab58079fbe
Talaibekov Erkinbek <32312803+ERERkin@users.noreply.github.com>
2022-03-04 20:10:29 +0600

    Init project

---
+++ src/main/java/kz/ccecc/hse_backend/HseBackendApplication.java
@@ -0,0 +1,13 @@
+package kz.ccecc.hse_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HseBackendApplication.class, args);
+	}
+
+}
